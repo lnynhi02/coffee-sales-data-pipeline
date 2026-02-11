183de8cd617f1728a56267c1deaa40c59ffcc7bc
lnynhi02 <ynnhi508@gmail.com>
2026-02-11 15:04:02 +0700

    fix: fix ending line script

--- docker_infra/kafka-connect/register.sh
+++ docker_infra/kafka-connect/register.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "Waiting for Kafka Connect..."
-until curl --fail -s http://kafka-connect:8083/connectors >/dev/null; do
+until curl -sf http://kafka-connect:8083/connectors >/dev/null; do
   echo "Waiting for Kafka Connect..."
   sleep 3
 done
 
 echo "Registering MySQL CDC connector..."
 
-curl -X POST http://kafka-connect:8083/connectors \
+if ! curl -sf -X POST http://kafka-connect:8083/connectors \
   -H "Content-Type: application/json" \
-  -d @/config/mysql-src-connector.json \
-|| echo "Connector already exists"
+  -d @/config/mysql-src-connector.json
+then
+  echo "Connector already exists"
+fi
